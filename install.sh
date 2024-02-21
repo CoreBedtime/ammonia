20680898bfde2a875581bab434a9dcc549419a21
CoreBedtime <121740653+CoreBedtime@users.noreply.github.com>
2024-02-21 16:31:13 -0500

    Install launch daemon

--- install.sh
+++ install.sh
@@ -12,3 +12,33 @@ cp ./fridagum.dylib /usr/local/bin/ammonia/
 mv ./Build/Release/ammonia /usr/local/bin/ammonia/
 mv ./Build/Release/liblibinfect.dylib /usr/local/bin/ammonia/
 mv ./Build/Release/libopener.dylib /usr/local/bin/ammonia/
+
+# Define the path to the plist file
+plist_file="/Library/LaunchDaemons/com.amm.loader.plist"
+
+# Check if the file exists
+if [ ! -f "$plist_file" ]; then
+    # Create the plist file if it doesn't exist
+    cat <<EOF | sudo tee "$plist_file" > /dev/null
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+<dict>
+    <key>Label</key>
+    <string>com.amm.loader</string>
+    <key>ProgramArguments</key>
+    <array>
+        <string>/usr/local/bin/ammonia/ammonia</string>
+    </array>
+    <key>RunAtLoad</key>
+    <true/>
+    <key>KeepAlive</key>
+    <false/>
+</dict>
+</plist>
+EOF
+fi
+
+# Load the launchd job
+launchctl load -w "$plist_file"
+
