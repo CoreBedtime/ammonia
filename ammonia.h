b690dd709f369af7b10a26a3fefc7bf438d7e2f4
CoreBedtime <121740653+CoreBedtime@users.noreply.github.com>
2024-03-29 12:24:32 -0400

    fix build (?)

---
+++ ammonia.h
@@ -0,0 +1,17 @@
+//
+//  rosetta.h
+//  ammonia
+//
+//  Created by bedtime on 11/19/23.
+//
+
+#ifndef rosetta_h
+#define rosetta_h
+
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define SupportFolderP "/usr/local/bin/ammonia/"
+
+#endif /* rosetta_h */
